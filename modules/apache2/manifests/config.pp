93588319da315153a01f8e6b870ff10dc8e84285
atmac <mac@admins-MacBook-Pro.local>
2014-09-16 22:52:57 +0100

    updated

--- modules/apache2/manifests/config.pp
+++ modules/apache2/manifests/config.pp
@@ -6,7 +6,7 @@ class apache2::config {
     owner   => $apache2::userName,
     group   => $apache2::groupName,
     mode    => 644,
-    before  => File[$apache2::configFile]
+   # before  => File[$apache2::configFile]
   }
 
   file { "${apache2::configDir}/${apache2::configFile}":
@@ -16,18 +16,18 @@ class apache2::config {
     mode    => 664,
     source  => "puppet:///modules/apache2/httpd.conf",
     require => File["${apache2::configDir}"],
-    before  => Service[$apache2::service],
+    #before  => Service[$apache2::service],
 
   }
 
-  user { "${apache2::userName}":
-    ensure      => present,
-    home        => "/home/$apache2::userName",
-    managehome  => true,
-    shell       => "/bin/bash",
-    comment     => "Some User Account",
-    before      => Package[$apache2::service]
-  }
+#  user { "${apache2::userName}":
+ #   ensure      => present,
+  #  home        => "/home/$apache2::userName",
+   # managehome  => true,
+    #shell       => "/bin/bash",
+   # comment     => "Some User Account",
+   
+  #}
 
 }
 
