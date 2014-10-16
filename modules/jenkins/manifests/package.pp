bc0c0792413389414b1796c68e9428dabdebba03
atmac <mac@admins-MacBook-Pro.local>
2014-10-16 22:24:07 +0100

    Jenkins update

--- modules/jenkins/manifests/package.pp
+++ modules/jenkins/manifests/package.pp
@@ -1,14 +1,23 @@
 class jenkins::package () {
 
+ 
+ exec { "create_jenkins_repo":
+    command => "wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo;sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key",
+    path    => "/usr/local/bin/:/bin/:/usr/bin/",
+    # path    => [ "/usr/local/bin/", "/bin/" ],  # alternative syntax
+}
+ 
+ 
   package { $jenkins::packages:
  #name   => "httpd.x86_64",
     ensure => "installed",
     provider  => yum,
+    require => Exec["create_jenkins_repo"],
 }
-exec { "install_jre":
-    command => "yum -y install jre",
-    path    => "/usr/bin/",
-}
+#exec { "install_jre":
+ #   command => "yum -y install jre",
+  #  path    => "/usr/bin/",
+#}
 
 exec { "create_jenkins_repo":
     command => "wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo;sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key",
@@ -16,7 +25,7 @@ exec { "create_jenkins_repo":
     # path    => [ "/usr/local/bin/", "/bin/" ],  # alternative syntax
 }
  #sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
-#sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+#sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key.
  
  
 
